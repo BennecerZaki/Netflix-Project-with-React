7185d8f9a0dd709e93f91c4a72b8ac8c60bea57e
BitsHeart <ga_bennecer@esi.dz>
2020-12-18 03:08:41 +0100

    feat: add the player component ⏯️

---
+++ src/components/player/index.js
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState } from "react";
+import ReactDOM from "react-dom";
+import { Button, Close, Container, Inner, Overlay } from "./styles/player";
+
+export const PlayerContext = createContext();
+
+export default function Player({ children, ...restProps }) {
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  return (
+    <PlayerContext.Provider value={{ showPlayer, setShowPlayer }}>
+      <Container {...restProps}>{children}</Container>
+    </PlayerContext.Provider>
+  );
+}
+
+Player.Video = function PlayerVideo({ src, ...restProps }) {
+  const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+  return showPlayer
+    ? ReactDOM.createPortal(
+        <Overlay onClick={() => setShowPlayer(false)} data-testid="player">
+          <Inner>
+            <video id="netflix-player" controls>
+              <source src={src} type="video/mp4" />
+            </video>
+            <Close />
+          </Inner>
+        </Overlay>,
+        document.body
+      )
+    : null;
+};
+
+Player.Button = function PlayerButton({ ...restProps }) {
+  const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+  return (
+    <Button onClick={() => setShowPlayer(!showPlayer)} {...restProps}>
+      Play
+    </Button>
+  );
+};
