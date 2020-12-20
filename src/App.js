ae247be83c8f1c1647b9237a8c55be2f7c9b21c9
BitsHeart <ga_bennecer@esi.dz>
2020-12-20 03:13:20 +0100

    cleanup : add extra file necesserly for the project ( Logo / App.js ... etc )

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { FaqsContainer, FooterContainer, JumbotronContainer } from "./containers";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListner } from "./hooks";
+import { Browse, Home, SignIn, SignUp } from "./pages";
 
 export default function App() {
+  const { user } = useAuthListner();
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGNIN}>
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGNUP}>
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
+    </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import { render } from "react-dom";
 import App from "./App";
 import "normalize.css";
 import { GlobalStyles } from "./GlobalStyles";
+import { firebase } from "./libs/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
 render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById("root")
 );
