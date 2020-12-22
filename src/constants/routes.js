ba171551efb9cb04373eaecf3bf118c8d847c68b
BitsHeart <ga_bennecer@esi.dz>
2020-12-22 05:09:32 +0100

    cleanup : move images/videos

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,4 @@
-export const HOME = "/";
-export const BROWSE = "/browse";
-export const SIGNUP = "/signup";
-export const SIGNIN = "/signin";
+export const HOME = "/Netflix-Project-with-React/";
+export const BROWSE = "/Netflix-Project-with-React/browse";
+export const SIGNUP = "/Netflix-Project-with-React/signup";
+export const SIGNIN = "/Netflix-Project-with-React/signin";
